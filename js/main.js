49eae07bcdf7c8472db8d28e3b83b1b0ff8a9ae1
Alejandro Vargas <cjc23@192.168.1.123>
2016-10-12 21:40:28 -0500

    check winner

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ game.board = [
   [null,null,null],
   [null,null,null]
 ];
+game.board.dom_ready = false;
 game.nextTurn = function(){
   if(game.turn === gamer1){
     game.turn = gamer2;
@@ -20,6 +21,43 @@ game.nextTurn = function(){
     game.turn = gamer1;
   }
   return game.turn;
+};
+game.checkWinner = function(){
+  if(game.board.dom_ready !== true){
+    return null;
+  }
+  //check rows
+  game.board.forEach(function(row){
+    if(row[0].textContent===row[1].textContent &&
+      row[1].textContent===row[2].textContent &&
+      row[0].textContent!==''){
+         game.winner = row[0].textContent;
+    }
+  });
+  //check columns
+  var i=0;
+  for(i=0;i<game.board.length;i++){
+    if(game.board[0][i].textContent === game.board[1][i].textContent &&
+      game.board[1][i].textContent === game.board[2][i].textContent &&
+      game.board[0][i].textContent !== ''){
+        game.winner = game.board[0][i].textContent;
+      }
+  }
+  //check diagonals
+  if(game.board[0][0].textContent === game.board[1][1].textContent &&
+    game.board[1][1].textContent === game.board[2][2].textContent &&
+    game.board[0][0].textContent !== ''){
+      game.winner = game.board[0][0].textContent;
+    }
+  if(game.board[0][2].textContent === game.board[1][1].textContent &&
+    game.board[1][1].textContent === game.board[2][0].textContent &&
+    game.board[0][2].textContent !== ''){
+      game.winner = game.board[0][2].textContent;
+    }
+    if(game.winner !==null){
+      console.log(game.winner);
+    }
+  //return the winner if there is one else return null
 };
 /*
  * END Game Object Definition
@@ -35,11 +73,15 @@ document.addEventListener("DOMContentLoaded",function(){
   game.board[0] = Array.from(row0);
   game.board[1] = Array.from(row1);
   game.board[2] = Array.from(row2);
+  game.board.dom_ready=true;
   //board ready
   game.board.forEach(function(row,index,board){
     //add click behavior per cell
     row.forEach(function(cell, index, row){
         cell.addEventListener("click",function(){
+          if(cell.textContent !== ""){
+            return; //exit condition
+          }
           cell.textContent = game.turn;
           if(game.nextTurn() === gamer1){
             playing_info[0].querySelector(".status").textContent = "Juega";
@@ -48,7 +90,7 @@ document.addEventListener("DOMContentLoaded",function(){
             playing_info[0].querySelector(".status").textContent = "Espera";
             playing_info[1].querySelector(".status").textContent = "Juega";
           }
-
+          game.checkWinner();
         });
     });//for each of row
   });//for each of board
